06866b228bf3122266ef49ab4f939ff00596bb63
Jatin Vaidya <jatin.vaidya@okta.com>
2024-03-06 13:36:22 +1100

    vercelize

--- api/index.js
+++ api/index.js
@@ -12,8 +12,8 @@ const app = express();
 app.get("/", (req, res) => res.send("Express on Vercel"));
 
 // view engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "jade");
+// app.set("views", path.join(__dirname, "views"));
+// app.set("view engine", "jade");
 
 app.use(logger("dev"));
 app.use(express.json());
